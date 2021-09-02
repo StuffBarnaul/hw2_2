c4bea85fc9c7a2c1ec2dc7ed0218442edabd8bd4
DorofeevAS <stuff77@mail.ru>
2021-09-02 22:25:31 +0300

    hw 2-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Lorem from 'react-lorem-component'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const data = [
+        {
+            title: 'Article1',
+            content: <Lorem count={1}/>
+        },
+        {
+            title: 'Article2',
+            content: <Lorem count={3}/>
+        },
+        {
+            title: 'Article3',
+            content: <Lorem count={1}/>
+        },
+    ]
+
+    const Card = ({title, content}) => {
+        return (
+            <div className="App-card">
+                <h1>{title}</h1>
+                <p>{content}</p>
+            </div>
+        )
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+            </header>
+            <article className="App-content">
+                {data.map(props => Card(props))}
+            </article>
+
+        </div>
+    );
 }
 
 export default App;
